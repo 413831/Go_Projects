a921d07e76ec6d8df419cb9b1b00e36ca1eea865
413831 <alepleins3pp@gmail.com>
2023-03-13 17:57:32 -0300

    Entities

--- Gin_framework/main.go
+++ Gin_framework/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"Gin_framework/controller"
+	"Gin_framework/service"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
 
 func main() {
-	server := gin.Default
+	server := gin.Default()
+
+	server.GET("/test", check)
+	server.GET("/", findAll)
+	server.POST("/save", save)
+
+	server.Run(":8080")
+}
+
+func save(ctx *gin.Context) {
+	ctx.JSON(200, videoController.Save(ctx))
+}
+
+func findAll(ctx *gin.Context) {
+	ctx.JSON(200, videoController.FindAll())
+}
+
+func check(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"message": "OK!!",
+	})
 }
